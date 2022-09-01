ac39378cced9c3e32298396a66cc5f329b81f95b
Mikhail Svarichevsky <3@14.by>
2022-09-01 23:10:18 +0200

    Add files via upload

--- src/user_module_341628725785264722.v
+++ src/user_module_341628725785264722.v
@@ -7,13 +7,29 @@ module user_module_341628725785264722(
   output [7:0] io_out
 );
 
-wire clk, rst_n;
+wire clk, rst_n, shift_clk, shift_dta;
 
 assign clk = io_in[0];
 assign rst_n = io_in[1];
+assign shift_clk = io_in[2];
+assign shift_dta = io_in[3];
+
+
+/*Shift register chain, 128-bit*/
+reg [127:0] shifter;
+
+always @(posedge shift_clk)
+begin
+    shifter[127:1] <= shifter[126:0];
+    shifter[0]   <= shift_dta;
+end
+
+
+
   
 reg [7 : 0] data;
 assign io_out = data;
+assign io_out[7] = shifter[127];
 
 always @ (posedge clk or posedge rst_n) begin
   if (rst_n) begin
@@ -22,6 +38,7 @@ always @ (posedge clk or posedge rst_n) begin
   else begin
     data <= data + 1'b1;
   end
-end    
+end
+    
 
 endmodule
