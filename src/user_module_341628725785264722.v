7d9b37decfb705968000caa3d8da3bfb7d5da006
Mikhail Svarichevsky <3@14.by>
2022-09-02 19:45:07 +0200

    Unique submodule name, div4
    div4->div4_341628725785264722

--- src/user_module_341628725785264722.v
+++ src/user_module_341628725785264722.v
@@ -2,7 +2,7 @@
 
 `default_nettype none
 
-module div4 ( clk ,rst, out_clk );
+module div4_341628725785264722 ( clk ,rst, out_clk );
     output out_clk;
     input clk ;
     input rst;
@@ -49,14 +49,14 @@ end
 //0
 wire c0_1 = clk;
 wire c0_output;
-div4 tmp0(c0_1, rst_n, c0_output);
+div4_341628725785264722 tmp0(c0_1, rst_n, c0_output);
 
 //1
 wire c1_1, c1_2, c1_3, c1_output;
 assign c1_1 = c1_3 ^ shifter[0];
 assign c1_2 = c1_1 ^ shifter[1];
 assign c1_3 = c1_2 ^ shifter[2];
-div4 tmp1(c1_3, rst_n, c1_output);
+div4_341628725785264722 tmp1(c1_3, rst_n, c1_output);
 
 //2
 wire c2_1, c2_2, c2_3, c2_4, c2_5, c2_output;
@@ -65,18 +65,18 @@ assign c2_2 = c2_1 ^ shifter[1];
 assign c2_3 = c2_2 ^ shifter[2];
 assign c2_4 = c2_3 ^ shifter[3];
 assign c2_5 = c2_4 ^ shifter[4];
-div4 tmp2(c2_5, rst_n, c2_output);
+div4_341628725785264722 tmp2(c2_5, rst_n, c2_output);
 
 //3
 wire c3_1, c3_output;
 assign c3_1 = c3_1 ^ shifter[0];
-div4 tmp3(c3_1, rst_n, c3_output);
+div4_341628725785264722 tmp3(c3_1, rst_n, c3_output);
 
 //4 - requires shifter configuration to convert one stage to buffer 
 wire c4_1, c4_2, c4_output;
 assign c4_1 = c4_2 ^ shifter[0];
 assign c4_2 = c4_1 ^ shifter[1];
-div4 tmp4(c4_2, rst_n, c4_output);
+div4_341628725785264722 tmp4(c4_2, rst_n, c4_output);
 
 //5 - NAND version
 wire c5_1, c5_2, c5_3, c5_4, c5_5, c5_output;
@@ -85,7 +85,7 @@ assign c5_2 = ~(c5_1 & shifter[1]);
 assign c5_3 = ~(c5_2 & shifter[2]);
 assign c5_4 = ~(c5_3 & shifter[3]);
 assign c5_5 = ~(c5_4 & shifter[4]);
-div4 tmp5(c5_5, rst_n, c5_output);
+div4_341628725785264722 tmp5(c5_5, rst_n, c5_output);
 
 //6 - NOR version
 wire c6_1, c6_2, c6_3, c6_4, c6_5, c6_output;
@@ -94,7 +94,7 @@ assign c6_2 = ~(c6_1 | shifter[1]);
 assign c6_3 = ~(c6_2 | shifter[2]);
 assign c6_4 = ~(c6_3 | shifter[3]);
 assign c6_5 = ~(c6_4 | shifter[4]);
-div4 tmp6(c6_5, rst_n, c6_output);
+div4_341628725785264722 tmp6(c6_5, rst_n, c6_output);
 
 //7 - + version
 wire c7_1, c7_2, c7_3, c7_4, c7_5, c7_output;
@@ -103,7 +103,7 @@ assign c7_2 = (c7_1 + shifter[2] + shifter[3]);
 assign c7_3 = (c7_2 + shifter[4] + shifter[5]);
 assign c7_4 = (c7_3 + shifter[6] + shifter[7]);
 assign c7_5 = (c7_4 + shifter[8] + shifter[9]);
-div4 tmp7(c7_5, rst_n, c7_output);
+div4_341628725785264722 tmp7(c7_5, rst_n, c7_output);
 
 /*Clock selector*/
 reg selected_clock;
@@ -129,7 +129,7 @@ assign io_out[4] = data[23];
 assign io_out[5] = data[27];
 assign io_out[6] = data[29];
 assign io_out[7] = shifter[11];
-//div4 tmp1(clk, rst_n, io_out[6]);
+//div4_341628725785264722 tmp1(clk, rst_n, io_out[6]);
 
 always @ (posedge selected_clock or posedge rst_n) begin
   if (rst_n) begin
